52c4f350c1cb01933919a5df669b0657dd30f233
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-09-01 00:40:05 +0200

    logic - impliment onSubmit login function and error handling

--- src/app/(auth)/login/LoginForm.tsx
+++ src/app/(auth)/login/LoginForm.tsx
@@ -8,14 +8,18 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import React from "react";
+import React, { useState, useTransition } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useForm } from "react-hook-form";
-import { loginSchema, LoginValues, SignUpValues } from "@/lib/validation";
+import { loginSchema, LoginValues } from "@/lib/validation";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { login } from "./actions";
 
 export default function LoginForm() {
+  const [error, setError] = useState<string>();
+  const [isPending, startTransition] = useTransition();
+
   const form = useForm<LoginValues>({
     resolver: zodResolver(loginSchema),
     defaultValues: {
@@ -24,9 +28,17 @@ export default function LoginForm() {
     },
   });
 
+  async function onSubmit(values: LoginValues) {
+    setError(undefined);
+    startTransition(async () => {
+      const { error } = await login(values);
+      if (error) setError(error);
+    });
+  }
+
   return (
     <Form {...form}>
-      <form className="space-y-3">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
         <FormField
           name="username"
           render={({ field }) => (
@@ -45,13 +57,15 @@ export default function LoginForm() {
             <FormItem>
               <FormLabel>Password</FormLabel>
               <FormControl>
-                <Input placeholder="Password" {...field} />
+                <Input type="password" placeholder="Password" {...field} />
               </FormControl>
               <FormMessage />
             </FormItem>
           )}
         />
-        <Button className="w-full">Log in</Button>
+        <Button type="submit" className="w-full">
+          Log in
+        </Button>
       </form>
     </Form>
   );
