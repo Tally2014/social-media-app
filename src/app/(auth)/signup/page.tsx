d326e79429de67e863732b186fa0edd3b1cc5904
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-08-31 23:47:40 +0200

    style - remove bg-gray-700 background color in signup page

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 };
 export default function Page() {
   return (
-    <main className="flex h-screen items-center justify-center bg-gray-700 p-5">
+    <main className="flex h-screen items-center justify-center p-5">
       <div className="flex h-full max-h-[40rem] w-full max-w-[64rem] overflow-hidden rounded-2xl bg-card shadow-2xl">
         <div className="w-full space-y-10 overflow-y-auto p-10 md:w-1/2">
           <div className="space-y-1 text-center">
