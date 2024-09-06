da085dfe62a5bcb7d6a97db7b5e5ef1792a4a9b9
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-09-06 18:17:24 +0200

    fix - correct submitPost typescript prop

--- src/components/posts/editor/actions.ts
+++ src/components/posts/editor/actions.ts
@@ -4,10 +4,7 @@ import { validateRequest } from "@/auth";
 import prisma from "@/lib/prisma";
 import { createPostSchema } from "@/lib/validation";
 
-export async function submitPost(input: {
-  content: string;
-  mediaIds: string[];
-}) {
+export async function submitPost(input: string) {
   const { user } = await validateRequest();
 
   if (!user) throw new Error("Unauthorized");
