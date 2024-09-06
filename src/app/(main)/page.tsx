ffa1c698adeb887289ef4da45aac7b9f11f271ef
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-09-06 18:52:37 +0200

    logic - fetch posts from primsa db and create a Post component and map them i n main page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,11 +1,28 @@
 import PostEditor from "@/components/posts/editor/PostEditor";
+import Post from "@/components/posts/Post";
+import prisma from "@/lib/prisma";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const posts = await prisma.post.findMany({
+    include: {
+      user: {
+        select: {
+          username: true,
+          displayName: true,
+          avatarUrl: true,
+        },
+      },
+    },
+    orderBy: { createdAt: "desc" },
+  });
   return (
     <main className="h-[200vh] w-full">
       <h1 className="w-full">
         <PostEditor />
+        {posts.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
       </h1>
     </main>
   );
