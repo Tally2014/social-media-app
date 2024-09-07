3cb43d4c11cb6ca423db30ef23792b601f45a329
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-09-07 23:40:22 +0200

    style - create usersToFollow styling

--- src/components/TrendsSidebar.tsx
+++ src/components/TrendsSidebar.tsx
@@ -1,12 +1,17 @@
 import { validateRequest } from "@/auth";
 import prisma from "@/lib/prisma";
 import { userDataSelect } from "@/lib/types";
-import React from "react";
+import { Loader2 } from "lucide-react";
+import Link from "next/link";
+import React, { Suspense } from "react";
+import UserAvatar from "./UserAvatar";
 
 export default function TrendsSidebar() {
   return (
     <div className="sticky top-[5.25rem] hidden h-fit w-72 flex-none space-y-5 md:block lg:w-80">
-      <WhoToFollow />
+      <Suspense fallback={<Loader2 className="mx-auto animate-spin" />}>
+        <WhoToFollow />
+      </Suspense>
     </div>
   );
 }
@@ -28,6 +33,24 @@ async function WhoToFollow() {
   return (
     <div className="space-y-5 rounded-2xl bg-card p-5 shadow-sm">
       <div className="text-xl font-bold">Who to follow</div>
+      {usersToFollow.map((user) => (
+        <div key={user.id} className="flex items-center justify-between gap-3">
+          <Link
+            href={`/users/${user.username}`}
+            className="flex items-center gap-3"
+          >
+            <UserAvatar avatarUrl={user.avatarUrl} className="flex-none" />
+            <div>
+              <p className="line-clamp-1 break-all font-semibold hover:underline">
+                {user.displayName}
+              </p>
+              <p className="line-clamp-1 break-all text-muted-foreground">
+                @{user.username}
+              </p>
+            </div>
+          </Link>
+        </div>
+      ))}
     </div>
   );
 }
