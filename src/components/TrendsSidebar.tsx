ad7a9cc1b51630827810cc1840b5280b747b7e06
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-09-07 23:16:34 +0200

    logic - setup who to follow right sidebar content

--- src/components/TrendsSidebar.tsx
+++ src/components/TrendsSidebar.tsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import { validateRequest } from "@/auth";
+import prisma from "@/lib/prisma";
+import { userDataSelect } from "@/lib/types";
+import React from "react";
 
 export default function TrendsSidebar() {
   return (
-    <div>TrendsSidebar</div>
-  )
+    <div className="sticky top-[5.25rem] hidden h-fit w-72 flex-none space-y-5 md:block lg:w-80">
+      <WhoToFollow />
+    </div>
+  );
+}
+
+async function WhoToFollow() {
+  const { user } = await validateRequest();
+  if (!user) return null;
+
+  const usersToFollow = await prisma.user.findMany({
+    where: {
+      NOT: {
+        id: user.id,
+      },
+    },
+    select: userDataSelect,
+    take: 5,
+  });
+
+  return (
+    <div className="space-y-5 rounded-2xl bg-card p-5 shadow-sm">
+      <div className="text-xl font-bold">Who to follow</div>
+    </div>
+  );
 }
