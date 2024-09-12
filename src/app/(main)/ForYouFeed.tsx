e28aa0a6266f7cbe2ee6234f3a44819fb1aef71e
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-09-12 14:26:42 +0200

    logic - useInfiniteQuery to load ForYouPost posts

--- src/app/(main)/ForYouFeed.tsx
+++ src/app/(main)/ForYouFeed.tsx
@@ -1,20 +1,48 @@
 "use client";
 
 import Post from "@/components/posts/Post";
+import { Button } from "@/components/ui/button";
 import kyInstance from "@/lib/ky";
-import { PostData } from "@/lib/types";
-import { useQuery } from "@tanstack/react-query";
+import { PostsPage } from "@/lib/types";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { Loader2 } from "lucide-react";
 
 export default function ForYouFeed() {
-  const query = useQuery<PostData[]>({
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery({
     queryKey: ["post-feed", "for-you"],
-    queryFn: kyInstance.get("/api/posts/for-you").json<PostData[]>,
+    queryFn: ({ pageParam }) =>
+      kyInstance
+        .get(
+          "/api/posts/for-you",
+          pageParam ? { searchParams: { cursor: pageParam } } : {},
+        )
+        .json<PostsPage>(),
+    initialPageParam: null as string | null,
+    getNextPageParam: (lastPage) => lastPage.nextCursor,
   });
-  if (query.status === "pending") {
+
+  const posts = data?.pages.flatMap((page) => page.posts) || [];
+
+  if (status === "pending") {
     return <Loader2 className="mx-auto animate-spin" />;
   }
-  if (query.status === "error") {
+
+  if (status === "success" && !posts.length && !hasNextPage) {
+    return (
+      <p className="text-center text-muted-foreground">
+        No one has posted anything yet.
+      </p>
+    );
+  }
+
+  if (status === "error") {
     return (
       <p className="text-center text-destructive">
         An error occurred while loading posts.
@@ -22,10 +50,12 @@ export default function ForYouFeed() {
     );
   }
   return (
-    <>
-      {query.data.map((post) => (
+    <div className="space-y-5">
+      {posts.map((post) => (
         <Post key={post.id} post={post} />
       ))}
-    </>
+
+      <Button onClick={() => fetchNextPage()}>Load more</Button>
+    </div>
   );
 }
