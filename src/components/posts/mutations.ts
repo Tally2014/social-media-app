31677c33e35284e66015f5d1a521ff6d6e5cb965
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-09-12 19:38:39 +0200

    logic - invalidateQueries using queryClient when loading data

--- src/components/posts/mutations.ts
+++ src/components/posts/mutations.ts
@@ -35,6 +35,14 @@ export function useSubmitPostMutation() {
           }
         },
       );
+
+      queryClient.invalidateQueries({
+        queryKey: queryFilter.queryKey,
+        predicate(query) {
+          return !query.state.data;
+        },
+      });
+
       toast({
         description: "Post Created",
       });
