aecc2e91e0db14964d694abe199a4d603bb5cab6
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-09-18 00:49:51 +0200

    style - create FollowButton

---
+++ src/components/FollowButton.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import useFollowerInfo from "@/hooks/useFollowerInfo";
+import kyInstance from "@/lib/ky";
+import { FollowerInfo } from "@/lib/types";
+import { QueryKey, useMutation, useQueryClient } from "@tanstack/react-query";
+import { Button } from "./ui/button";
+import { useToast } from "./ui/use-toast";
+
+interface FollowButtonProps {
+  userId: string;
+  initialState: FollowerInfo;
+}
+
+export default function FollowButton({
+  userId,
+  initialState,
+}: FollowButtonProps) {
+  const { toast } = useToast();
+
+  const queryClient = useQueryClient();
+
+  const { data } = useFollowerInfo(userId, initialState);
+
+  const queryKey: QueryKey = ["follower-info", userId];
+
+  const { mutate } = useMutation({
+    mutationFn: () =>
+      data.isFollowedByUser
+        ? kyInstance.delete(`/api/users/${userId}/followers`)
+        : kyInstance.post(`/api/users/${userId}/followers`),
+  });
+
+  return (
+    <Button
+      variant={data.isFollowedByUser ? "secondary" : "default"}
+      onClick={() => mutate()}
+    >
+      {data.isFollowedByUser ? "Unfollow" : "Follow"}
+    </Button>
+  );
+}
